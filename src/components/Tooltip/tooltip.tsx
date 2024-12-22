5c0c6adba06b66eae27f15efaceb03afbecef99a
nielpd <daniel.batista@pditabira.com>
2024-12-21 21:11:26 -0300

    feat: add tooltip component

---
+++ src/components/Tooltip/tooltip.tsx
@@ -0,0 +1,16 @@
+export default function Tooltip({
+  text,
+  children,
+}: {
+  text: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex justify-center items-center group">
+      {children}
+      <span className="absolute bottom-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">
+        {text}
+      </span>
+    </div>
+  );
+}
