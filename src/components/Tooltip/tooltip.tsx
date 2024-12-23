7a10eabccad26f4e546ac23891ee1cd2813527c9
nielpd <daniel.batista@pditabira.com>
2024-12-22 21:54:04 -0300

    feat: update tooltip component (text center)

--- src/components/Tooltip/tooltip.tsx
+++ src/components/Tooltip/tooltip.tsx
@@ -6,7 +6,7 @@ export default function Tooltip({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex justify-center items-center group">
+    <div className="relative flex justify-center items-center group text-center">
       {children}
       <span className="absolute bottom-10 scale-0 transition-all rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100">
         {text}
